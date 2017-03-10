d9382cd59f0828b2bddb0c729f657e30d2bcd555
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-03-10 17:47:50 -0500

    adding divs to represent the numbers on the face

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 
 let timer;
-let degMin = 0,
-    degHr = 0,
+let degMin = 180,
+    degHr = 180,
     degSec = 0;
+
+let time = new Date();
+
+
 
 
 function startClock(){
-	timer = window.setInterval(moveHands, 800);
+	timer = window.setInterval(moveHands, 1000);
 }
 
 function moveHands(){
-	$('#min').css('transform', 'rotate('+degMin+'deg)');
-    degMin += 90;
+	// $('#min').css('transform', 'rotate('+degMin+'deg)');
+ //    degMin += 90;
 
-    $('#hour').css('transform', 'rotate('+degHr+'deg)');
-    degHr += 45;
+ //    $('#hour').css('transform', 'rotate('+degHr+'deg)');
+ //    degHr += 45;
+ 	let sec = time.getSeconds();
 
     $('#sec').css('transform', 'rotate('+degSec+'deg)');
     degSec += 30;
