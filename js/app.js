ea47de4cd0ce6238791a43d71a2409980d402671
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-03-12 17:41:46 -0400

    hands are representing real time

--- js/app.js
+++ js/app.js
@@ -2,30 +2,44 @@
 let timer;
 let degMin = 180,
     degHr = 180,
-    degSec = 0;
-
-let time = new Date();
-
-
+    degSec = 180;
 
 
 function startClock(){
-	timer = window.setInterval(moveHands, 1000);
+	timer = window.setInterval(moveHands, 500);
 }
 
 function moveHands(){
-	// $('#min').css('transform', 'rotate('+degMin+'deg)');
- //    degMin += 90;
+	
+	let time = new Date();
 
- //    $('#hour').css('transform', 'rotate('+degHr+'deg)');
- //    degHr += 45;
- 	let sec = time.getSeconds();
+	let hr = time.getHours();
+	let min = time.getMinutes();
+	let sec = time.getSeconds();
+
+ 	//console.log("hr: " + hr);
+
+ 	//getting hour number
+ 	degHr = 180 + 30 * (hr - 12);
+ 	//taking into account where the min hand is to position the hr hand
+ 	//hr hand moves .5 deg per min
+ 	degHr += min * .5;
+
+    $('#hour').css('transform', 'rotate('+degHr+'deg)');
+
+ 	
+ 	//console.log("min: " + min);
+
+ 	degMin = 180 + 30 * (min/5);
+
+ 	$('#min').css('transform', 'rotate('+degMin+'deg)');
+
+ 	
+ 	//console.log("sec: " + sec);
+
+ 	degSec = 180 + 6 * sec;
 
     $('#sec').css('transform', 'rotate('+degSec+'deg)');
-    degSec += 30;
-    
 }
-
-
 
 window.addEventListener('load', startClock);
